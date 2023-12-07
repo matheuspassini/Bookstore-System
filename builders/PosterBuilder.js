2c522e6c176e1d606ee718f87469f5c74e5cfca3
Matheus Passini <80512548+matheuspassini@users.noreply.github.com>
2023-12-07 14:47:12 -0300

    Update PosterBuilder.js

--- builders/PosterBuilder.js
+++ builders/PosterBuilder.js
@@ -1,11 +1,50 @@
 const Poster = require("../entities/Poster")
+const ProductBuilder = require("./ProductBuilder")
 
-module.exports = class PosterBuilder {
-    constructor(name, description, height, width, price, inStock = 0) {
-        this.poster = new Poster(name, description, height, width, price, inStock)
+module.exports = class PosterBuilder extends ProductBuilder{
+
+    constructor() {
+        super()
+    }
+
+    setName(name) {
+        this.name = name
+        return this
+    }
+
+    setDescription(description) {
+        this.description = description
+        return this
+    }
+
+    setHeight(height) {
+        this.height = height
+        return this
+    }
+
+    setWidth(width) {
+        this.width = width
+        return this
+    }
+
+    setPrice(price) {
+        this.price = price
+        return this
+    }
+
+    setInStock(inStock) {
+        this.inStock = inStock
+        return this
     }
 
     build() {
-        return this.poster
+        return new Poster(
+            this.name,
+            this.description,
+            this.height,
+            this.width,
+            this.price,
+            this.inStock
+        )
     }
 }
