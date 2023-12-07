9a6f0d71f7958a605c33fba17d6b7e2bc657fa7c
Matheus Passini <80512548+matheuspassini@users.noreply.github.com>
2023-12-07 14:46:39 -0300

    Update BookBuilder.js

--- builders/BookBuilder.js
+++ builders/BookBuilder.js
@@ -1,11 +1,62 @@
 const Book = require("../entities/Book")
+const ProductBuilder = require("./ProductBuilder")
 
-module.exports = class BookBuilder {
-    constructor(title, synopsis, genre, pages, author, description, price, inStock) {
-        this.book = new Book(title, synopsis, genre, pages, author, description, price, inStock);
+module.exports = class BookBuilder extends ProductBuilder {
+
+    constructor() {
+        super()
+    }
+
+    setTitle(title) {
+        this.title = title
+        return this
+    }
+
+    setSynopsis(synopsis) {
+        this.synopsis = synopsis
+        return this
+    }
+
+    setPages(pages) {
+        this.pages = pages
+        return this
+    }
+
+    setGenre(genre) {
+        this.genre = genre
+        return this
+    }
+
+    setAuthor(author) {
+        this.author = author
+        return this
+    }
+
+    setDescription(description) {
+        this.description = description
+        return this
+    }
+
+    setPrice(price) {
+        this.price = price
+        return this
+    }
+
+    setInStock(inStock) {
+        this.inStock = inStock
+        return this
     }
 
     build() {
-        return this.book;
+        return new Book(
+            this.title,
+            this.synopsis,
+            this.pages,
+            this.genre,
+            this.author,
+            this.description,
+            this.price,
+            this.inStock
+        )
     }
 }
